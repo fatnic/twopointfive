9fcaceddf0fb2b29400a3a7ccd4bc2bcbf2dac35
Dave Nicholson <fatnic388@gmail.com>
2017-03-28 20:51:43 +0100

    spinning dot thing

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local fl = 300
-local stars = {}
-local numStars = 21
-local cZ = 1300
+local dots = {}
+local numDots = 200
+local cZ = 3000
 local radius = 1000
 local baseAngle = 0
 local rotationSpeed = 0.01
@@ -10,15 +10,14 @@ local yPos = 0
 function love.load()
     love.graphics.setColor(255, 255, 255)
 
-    for i=0, numStars-1 do
-        local star = {
-            y = 0,
-            angle = (math.pi * 2 / numStars) * i,
-            image = love.graphics.newImage('star.png')
+    for i=0, numDots-1 do
+        local dot = {
+            angle = 0.2 * i,
+            y = 3000 - 6000 / numDots * i
         }
-        star.x = math.cos(star.angle + baseAngle) * radius
-        star.z = cZ + math.sin(star.angle + baseAngle) * radius
-        table.insert(stars, star)
+        dot.x = math.cos(dot.angle + baseAngle) * radius
+        dot.z = cZ + math.sin(dot.angle + baseAngle) * radius
+        table.insert(dots, dot)
     end
 end
 
@@ -31,20 +30,18 @@ function love.draw()
     baseAngle = baseAngle + rotationSpeed
 
     love.graphics.translate(love.graphics:getWidth() / 2, love.graphics:getHeight() / 2)
-    table.sort(stars, function(a,b) return a.z > b.z end)
 
-    for _, star in pairs(stars) do
-        local perspective = fl / (fl + star.z)
-        star.y = yPos
+    for _, dot in pairs(dots) do
+        local perspective = fl / (fl + dot.z)
 
         love.graphics.push()
         love.graphics.scale(perspective, perspective)
-        love.graphics.translate(star.x, star.y)
-        love.graphics.translate(-star.image:getWidth() / 2, -star.image:getHeight() / 2)
-        love.graphics.draw(star.image, 0, 0)  
+        love.graphics.translate(dot.x, dot.y)
+        
+        love.graphics.circle('fill', 0, 0, 40)  
         love.graphics.pop()
 
-        star.x = math.cos(star.angle + baseAngle) * radius
-        star.z = cZ + math.sin(star.angle + baseAngle) * radius
+        dot.x = math.cos(dot.angle + baseAngle) * radius
+        dot.z = cZ + math.sin(dot.angle + baseAngle) * radius
     end
 end
